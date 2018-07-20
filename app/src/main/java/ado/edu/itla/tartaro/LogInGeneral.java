3aec601f53bef7bfa6dac501d3a41ecc55bf64aa
Marcos Ruiz C <marcosruizc.1@gmail.com>
2018-07-19 20:49:43 -0400

    Inicio de LogIn Tartaro

---
+++ app/src/main/java/ado/edu/itla/tartaro/LogInGeneral.java
@@ -0,0 +1,13 @@
+package ado.edu.itla.tartaro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LogInGeneral extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_principal);
+    }
+}
