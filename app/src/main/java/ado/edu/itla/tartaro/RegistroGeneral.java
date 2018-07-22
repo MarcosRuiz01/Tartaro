ee98a216dbd8742ad286487ade2da0e040cea729
Marcos Ruiz C <marcosruizc.1@gmail.com>
2018-07-22 00:02:19 -0400

    Se corrigio los errores y se llevo hasta las condiciones de los radiosButton del registro... Continuar desde ahi

--- app/src/main/java/ado/edu/itla/tartaro/RegistroGeneral.java
+++ app/src/main/java/ado/edu/itla/tartaro/RegistroGeneral.java
@@ -1,6 +1,7 @@
 package ado.edu.itla.tartaro;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -35,46 +36,75 @@ public class RegistroGeneral extends AppCompatActivity {
         confPassw = findViewById(R.id.txt_conf_pass);
         rbtTecnico = findViewById(R.id.rbt_tecnico);
         rbtNormal = findViewById(R.id.rbt_normal);
+        rbtNormal.isChecked();
 
 
         Button btnRegistrarUsuario = findViewById(R.id.btn_registrar);
-        btnRegistrarUsuario.setOnClickListener(new View.OnClickListener() {
-
+      btnRegistrarUsuario.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                    Usuario user = new Usuario();
-                    user.setNombre(userName.getText().toString());
-                    user.setEmail(email.getText().toString());
-                    user.setPassword(passw.getText().toString());
+                Usuario user = new Usuario();
+                user.setNombre(userName.getText().toString());
+                user.setEmail(email.getText().toString());
+                user.setPassword(passw.getText().toString());
 
-                    String p1 = passw.getText().toString();
-                    String p2  = confPassw.getText().toString();
+                String nombreUser = userName.getText().toString();
+                String emailUser = email.getText().toString();
+                String p1 = passw.getText().toString();
+                String p2 = confPassw.getText().toString();
+
+                Intent LogIn = new Intent(RegistroGeneral.this, LogInGeneral.class);
+
+                if(!nombreUser.isEmpty()&&!emailUser.isEmpty()&&!p1.isEmpty()&&!p2.isEmpty()) {
 
 
-                if(p1.equals(p2)){
+                if (p1.equals(p2)) {
 
-                    if (rbtNormal.isChecked() == true) {
+                    if (rbtNormal.isChecked()==true) {
                         tipoUsuario = Usuario.TipoUsuario.NORMAL;
 
                         Log.i("REGISTRO USER NORMAL", user.toString());
+                        Toast.makeText(RegistroGeneral.this, "Registro TECNICO exitoso", Toast.LENGTH_SHORT).show();
 
-                    } else if (rbtTecnico.isChecked() == true) {
+//                        startActivity(LogIn);
+
+                    } else if (rbtTecnico.isChecked()==true) {
                         tipoUsuario = Usuario.TipoUsuario.TECNICO;
 
                         Log.i("REGISTRO USER TECNICO", user.toString());
+                        Toast.makeText(RegistroGeneral.this, "Registro NORMAL exitoso", Toast.LENGTH_SHORT).show();
 
-                    } else if (rbtTecnico.isChecked() == false && rbtNormal.isChecked() == false) {
-                        Toast.makeText(RegistroGeneral.this, "Es necesario que seleccione un tipo de usuario", Toast.LENGTH_SHORT).show();
+//                        startActivity(LogIn);
+
+                    } else {
+                        Toast.makeText(RegistroGeneral.this, "Debes elegir un Tipo de Usuario", Toast.LENGTH_SHORT).show();
                     }
 
-                        userRepo.guardar(user);
-                        Log.i("Registro user exitoso", user.toString());
+                    userRepo.guardar(user);
+                    Log.i("Registro user exitoso", user.toString());
 
 
                 } else {
-                    Toast.makeText(RegistroGeneral.this,"No se pudo confirmar las contraseñas, por favor intente denuevo",Toast.LENGTH_LONG).show();
-                }
+                    Toast.makeText(RegistroGeneral.this, "No se pudo confirmar las contraseñas, por favor intente denuevo", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(RegistroGeneral.this, "Debe llenar todos los campos", Toast.LENGTH_SHORT).show();
+                    }
+            }
+
+
+        });
+
+
+
+        Button btnCancelarRegistro = findViewById(R.id.btn_cancelar);
+        btnCancelarRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                finish();
+
             }
         });
 
--- app/src/main/java/ado/edu/itla/tartaro/repositorio/TareaRepositorio.java
+++ app/src/main/java/ado/edu/itla/tartaro/repositorio/TareaRepositorio.java
@@ -2,12 +2,15 @@ package ado.edu.itla.tartaro.repositorio;
 
 import java.util.List;
 
+import ado.edu.itla.tartaro.entidad.Categoria;
 import ado.edu.itla.tartaro.entidad.Tarea;
+import ado.edu.itla.tartaro.entidad.Usuario;
 
 public interface TareaRepositorio {
 
     boolean guardar(Tarea tarea);
     Tarea buscar(String nombre);
     Tarea buscar(int id);
-    List<Tarea> buscarTarea (String buscar);
+    List<Tarea> buscarAsignadaA (Usuario usuario);
+    List<Tarea> buscarCreadaPor (Usuario usuario);
 }
